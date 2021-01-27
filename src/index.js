2741a7e7cffc8b61c7289b6eefe7dc0ffcf7aae0
John Ratana <jratana1@gmail.com>
2021-01-27 14:42:34 -0500

    done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelectorAll("input")[1].addEventListener("click", function(event){
+    event.preventDefault()
+    
+    let button = document.createElement('button')
+    button.innerHTML = 'X'
+    button.addEventListener("click", (e)=>{
+      e.target.parentElement.remove();
+    });
+
+    let parent =document.querySelector('ul')
+    let task = document.createElement('li')
+    
+    task.innerHTML = document.querySelectorAll("input")[0].value
+    task.append(button)
+    parent.append(task)
+  });
 });
